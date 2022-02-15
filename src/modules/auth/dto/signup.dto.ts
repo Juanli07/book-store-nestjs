8574291ffac40ad619716369a10c208d60ac169a
Juan A. López Ix <juan07alberto@gmail.com>
2022-02-15 16:14:27 -0600

    Auth module

---
+++ src/modules/auth/dto/signup.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class SignupDto {
+
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+    
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    
+}
